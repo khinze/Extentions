f2ed5a6b1a34378a6e280a86f4188f0716ba47f8
khinze <kehinze@gmail.com>
2015-03-01 12:17:19 +0200

    added signalr and web api sentry clients

---
+++ src/Extentions/Extentions.SignalR.Sentry/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Microsoft.AspNet.SignalR.Hubs;
+using SharpRaven;
+
+namespace Extentions.SignalR.Sentry
+{
+    public class SentryErrorHandlingPipelineModule : HubPipelineModule
+    {
+        protected override void OnIncomingError(ExceptionContext exceptionContext, IHubIncomingInvokerContext invokerContext)
+        {
+            var ravenClient = new RavenClient(GetSentryDns());
+
+            ravenClient.CaptureException(exceptionContext.Error);
+
+            base.OnIncomingError(exceptionContext, invokerContext);
+        }
+
+        private static string GetSentryDns()
+        {
+            return ConfigurationManager.AppSettings["sentryDns"];
+        }
+    }
+}
---
+++ src/Extentions/Extentions.WebApi.Sentry/SentryWebApiErrorAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+using SharpRaven;
+
+namespace Extentions.WebApi.Sentry
+{
+    public class SentryApiErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            var ravenClient = new RavenClient(GetSentryDns());
+
+            var request = context.ActionContext.Request;
+
+            if (context.Exception == null)
+            {
+                throw new InvalidOperationException("Exception cannot be null..");
+            }
+
+            ravenClient.CaptureException(context.Exception);
+
+            var response = new
+            {
+                Message = "An internal system error has occured.",
+                AddtionalMessage = context.Exception.Message
+            };
+
+            context.Response = request.CreateResponse(HttpStatusCode.InternalServerError, response);
+        }
+
+        private static string GetSentryDns()
+        {
+            return ConfigurationManager.AppSettings["sentryDns"];    
+        }
+    }
+}
